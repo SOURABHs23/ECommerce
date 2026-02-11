b72fbc07750532cae39690166f98296fcef90b25
Sourabh Garg <Sourabhgarg873@gmail.com>
2026-02-11 22:37:31 +0530

    refactor: Migrate .env file loading to Spring Boot's native `spring.config.import` and update Lombok configuration.

--- backend/src/main/java/com/ecommerce/ECommerceApplication.java
+++ backend/src/main/java/com/ecommerce/ECommerceApplication.java
@@ -1,25 +1,19 @@
 package com.ecommerce;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
-import com.ecommerce.config.DotenvConfig;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
+@Slf4j
 public class ECommerceApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(ECommerceApplication.class);
-
     public static void main(String[] args) {
-        // Create Spring Application and add .env file loader
-        SpringApplication app = new SpringApplication(ECommerceApplication.class);
-        app.addInitializers(new DotenvConfig());
-        app.run(args);
-        logger.info("ECommerce Backend is running on http://localhost:8080");
+        SpringApplication.run(ECommerceApplication.class, args);
+        log.info("ECommerce Backend is running on http://localhost:8080");
     }
 }
